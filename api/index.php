8efe1111daede9d298d21cc1e292e30f8784643d
pendri13-crypto <pendri13@gmail.com>
2026-09-22 17:53:11 +0700

    fix vercel 500 error by redirecting view compiled path to /tmp

--- api/index.php
+++ api/index.php
@@ -1,4 +1,12 @@
 <?php
 
-// Forward Vercel requests to normal index.php
+// Vercel serverless environment is read-only.
+// We must redirect view compilation to /tmp
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
+$_SERVER['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
+
+if (!is_dir($_ENV['VIEW_COMPILED_PATH'])) {
+    mkdir($_ENV['VIEW_COMPILED_PATH'], 0777, true);
+}
+
 require __DIR__ . '/../public/index.php';
